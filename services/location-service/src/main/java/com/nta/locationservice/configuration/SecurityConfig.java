1792ea670d89d2633c1f829b96e534bdef65802a
ntheanh <theanhmgt66@gmail.com>
2024-11-17 13:25:59 +0700

    feat(api): expose APIs for creating a post and finding a post by ID through the gateway

--- services/location-service/src/main/java/com/nta/locationservice/configuration/SecurityConfig.java
+++ services/location-service/src/main/java/com/nta/locationservice/configuration/SecurityConfig.java
@@ -17,7 +17,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity
 public class SecurityConfig {
 
-    private static final String[] PUBLIC_ENDPOINTS = {"/internal/delivery-locations", "/internal/delivery-locations/**"};
+    private static final String[] PUBLIC_ENDPOINTS = {};
 
     private final CustomJwtDecoder customJwtDecoder;
 
@@ -29,7 +29,6 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeHttpRequests(request -> request.requestMatchers(HttpMethod.POST, PUBLIC_ENDPOINTS)
                 .permitAll()
-                .requestMatchers(HttpMethod.GET, PUBLIC_ENDPOINTS).permitAll()
                 .anyRequest()
                 .authenticated());
 
--- services/post-service/src/main/java/com/nta/postservice/configuration/SecurityConfig.java
+++ services/post-service/src/main/java/com/nta/postservice/configuration/SecurityConfig.java
@@ -16,7 +16,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity
 public class SecurityConfig {
 
-    private static final String[] PUBLIC_ENDPOINTS = {"/posts", "/posts/**"};
+    private static final String[] PUBLIC_ENDPOINTS = {};
 
     private final CustomJwtDecoder customJwtDecoder;
 
@@ -27,8 +27,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeHttpRequests(request -> request.requestMatchers(HttpMethod.POST, PUBLIC_ENDPOINTS)
-                .permitAll()
-                .requestMatchers(HttpMethod.GET, PUBLIC_ENDPOINTS)
                 .permitAll()
                 .anyRequest()
                 .authenticated());
